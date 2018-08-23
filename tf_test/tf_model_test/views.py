935a65186cf5627ed05c89c5175f1bd9a411fc07
David Joy <30907520+david7joy@users.noreply.github.com>
2018-08-23 00:50:08 -0500

    Modifying the application

--- tf_test/tf_model_test/views.py
+++ tf_test/tf_model_test/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from predict_client.prod_client import ProdClient
 
 # Create your views here.
 
 def test(request):
-    return render(request, 'tf_model_test/test.html')
+    client = ProdClient('localhost:9000', 'simple',2)
+    req_data = [{'in_tensor_name': 'inputs', 'in_tensor_dtype': 'DT_INT32', 'data': 5}]
+    value = client.predict(req_data)
+    print(value)
+    return render(request, 'tf_model_test/test.html',{'value':value})
+
